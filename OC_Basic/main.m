02ad7aa6a6a74a4b50052eff0cb636a9950adabc
YunTu <yuntu@YunTudeMac-mini.local>
2015-08-29 17:39:09 +0800

    Initial Commit

---
+++ OC_Basic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_Basic
+//
+//  Created by YunTu on 15/8/29.
+//  Copyright (c) 2015年 YunTu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
